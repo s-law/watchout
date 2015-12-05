60d434563daa84245cc768793f9c1ce05b47b4f2
s-law <slaw@nuclearanvil.com>
2015-12-05 13:07:14 -0800

    Collision checking now working. Possible bug in drag handler.

--- client/watchout.js
+++ client/watchout.js
@@ -6,7 +6,7 @@ var gameOptions = {
   padding: 20
 };
 var gameStats = {
-  current: 1, 
+  current: 0, 
   highscore: 0,
   collisions: 0
 };
@@ -21,6 +21,7 @@ var rToV = {
 
 var gameBoard = d3.select('.board')
   .append('svg')
+  .attr("class", "gameBoard")
   .attr("height", gameOptions.height)
   .attr("width", gameOptions.width)
 
@@ -45,36 +46,82 @@ var createEnemies = function() {
     randLoc = randomCoor();
     arrEne.push({id: i,
       x: randLoc[0],
-      y: randLoc[1]
+      y: randLoc[1],
+      r: 10
     });
   }
   return arrEne;
 };
 
-var render = function(arr) {
-  var allEne = d3.select('svg').selectAll("circle.enemies")
+var enemiesStepPosition = function(enemyObj, timestep) {
+  var startX = enemyObj.x;
+  var startY = enemyObj.y;
+  var randLocEnd = randomCoor();
+  var endX = randLocEnd[0];
+  var endY = randLocEnd[1];
+  var enemiesNextLocX = startX + (endX-startX)*timestep;
+  var enemiesNextLocY = startY + (endY-startY)*timestep;
+  enemyObj.x = enemiesNextLocX;
+  enemyObj.y = enemiesNextLocY;
+  return [enemiesNextLocX, enemiesNextLocY];  
+}
+
+var render = function(arr, player) {
+
+  var allEne = d3.select('svg.enemy').selectAll("circle.enemies")
     .data(arr, function(d) {
       return d.id;
     });
-  allEne.transition(3000)
-    .attr("cx", function(d) {return d.x})
-    .attr("cy", function(d) {return d.y});
 
   allEne.enter()
     .append("svg:circle")
     .attr("class", "enemies")
     .attr("cx", function(d) {return d.x})
     .attr("cy", function(d) {return d.y})
-    .attr("r", 10);
+    .attr("r", function(d) {return d.r});
 
+  allEne.transition().duration(1000).tween('custom', function() {
+    return function(t) {
+      updateEnemiesAndCollisionCheck(t);
+    }
+  })
+    .attr("cx", function(d) {return d.x})
+    .attr("cy", function(d) {return d.y});
+
+  var updateEnemiesAndCollisionCheck = function(t) {
+    var newPos = [];
+    for (var i = 0; i < arr.length; i++) {
+      newPos.push(enemiesStepPosition(arr[i], t))
+      if (i === 10 && t % 4 === 0) {
+        console.log(newPos[i][0], newPos[i][1]);
+      }
+    }
+
+    // check collision on these positions
+    for (var i = 0; i < arr.length; i++) {
+      var collisionDist = player.r + arr[i].r;
+
+      if ( Math.pow((player.xPos - arr[i].x), 2) + Math.pow((player.yPos - arr[i].y), 2) < Math.pow(collisionDist, 2)) {
+        gameStats.current = 0;
+        gameStats.collisions++;
+        changeStats();
+        changeStats('collisions');
+        changeBestScore();
+      }
+    };
+  };
 };
 
 var runGame = function() {
-  var test = new Player();
-  test.dragPlayer();
+  var player = new Player();
+  var arr = createEnemies();
+  d3.select("svg.gameBoard")
+    .append('svg')
+    .attr("class", "enemy");
+  
   setInterval(function() {
-    var arr = createEnemies();
-    render(arr);
+    render(arr, player);
+    console.log("Player: ", player.xPos, player.yPos);
   }, 1000);
 
 };
