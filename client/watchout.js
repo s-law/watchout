410e8359c7008b8f6c934c93a337f50758ea5d22
s-law <slaw@nuclearanvil.com>
2015-12-04 20:35:05 -0800

    Test function to enabling cycling.

--- client/watchout.js
+++ client/watchout.js
@@ -14,6 +14,10 @@ var gameBoardAxes = {
   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
 };
+var rToV = {
+  x: d3.scale.linear().domain([0,gameOptions.width]).range([0,100]),
+  y: d3.scale.linear().domain([0,gameOptions.height]).range([0,100])
+};
 
 var gameBoard = d3.select('.board')
   .append('svg')
@@ -36,10 +40,13 @@ var changeBestScore = function() {
 
 var createEnemies = function() {
   var arrEne = [];
+  var randLoc;
   for (var i=0; i<gameOptions.nEnemies; i++) {
+    randLoc = randomCoor();
     arrEne.push({id: i,
-      x: gameBoardAxes.x(Math.random()*100),
-      y: gameBoardAxes.y(Math.random()*100)});
+      x: randLoc[0],
+      y: randLoc[1]
+    });
   }
   return arrEne;
 };
@@ -49,14 +56,38 @@ var render = function(arr) {
     .data(arr, function(d) {
       return d.id;
     });
-  
+  allEne.transition(3000)
+    .attr("cx", function(d) {return d.x})
+    .attr("cy", function(d) {return d.y});
+
   allEne.enter()
     .append("svg:circle")
     .attr("class", "enemies")
     .attr("cx", function(d) {return d.x})
     .attr("cy", function(d) {return d.y})
     .attr("r", 10);
+
 };
 
+var runGame = function() {
+  var test = new Player();
+  test.dragPlayer();
+  setInterval(function() {
+    var arr = createEnemies();
+    render(arr);
+  }, 1000);
+
+};
+
+var randomCoor = function() {
+  var x = gameBoardAxes.x(Math.random()*100);
+  var y = gameBoardAxes.y(Math.random()*100);
+  return [x, y];
+};
+
+// var collCheck = function() {
+
+//   //for (var i=0; i)
+// };
 
 
